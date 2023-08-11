9d1b6e6e13a5d6ad928a4897a06ca645d3dfdf57
Alex Dickinson <nad38@cantab.ac.uk>
2023-08-11 11:28:04 +0000

    correcting roxygen comment for stretch_recall_time_lever()

--- R/levers.R
+++ R/levers.R
@@ -158,7 +158,7 @@ stretch_profiles <- function(profiles, lever_profiles_det_stretch_factors, min_s
 
 #' Stretch time on recall by a user-defined factor
 #' 
-#'#' @export
+#' @export
 stretch_recall_time_lever <- function(recall_time, lever_profiles_recall_stretch_factors, min_stretch_factor, max_stretch_factor) {
   
   for (stretch_factor_name in names(lever_profiles_recall_stretch_factors)) {
